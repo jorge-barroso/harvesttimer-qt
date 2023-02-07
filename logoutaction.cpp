f21649bf64418d1a5f22971711b359062999b43b
jorge-barroso <jorge_barroso_11@hotmail.com>
2023-02-07 00:36:37 +0000

    Updated logout icon

--- logoutaction.cpp
+++ logoutaction.cpp
@@ -6,6 +6,6 @@
 #include<QApplication>
 
 LogoutAction::LogoutAction(QObject *parent)
-    : QAction(QIcon::fromTheme("system-log-out"), QApplication::translate("Menu", "Logout"), parent) {
+    : QAction(QIcon::fromTheme("list-remove-user"), QApplication::translate("Menu", "Logout"), parent) {
     this->setStatusTip(QApplication::translate("Menu", "Logs you out from Harvest and closes the application"));
 }
